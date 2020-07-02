5244f3519057e492518da35a3c2b9e7938f9ee16
Kayan <kayanleye@gmail.com>
2020-07-02 18:26:49 +0100

    image adjusted

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1208,7 +1208,7 @@ export default {
             overflow: hidden;
             .card-img {
               height: auto !important;
-              width: 10% !important;
+              width: 100% !important;
               margin: 0;
               border-radius: 0;
               margin-bottom: 0 !important;
